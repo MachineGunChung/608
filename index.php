e940ad8f3cde81f77afcc5087cba9f531d0f8e8d
JohnnyG <johngardnernz@gmail.com>
2025-01-28 13:14:17 +1300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Healthy Life Clinic</title>
+    <link rel="stylesheet" href="original_template/style.css">
+</head>
+<body>
+    <h1>Welcome to Healthy Life Clinic</h1>
+    <nav>
+        <ul>
+            <li><a href="original_template/index.html">Original Template</a></li>
+            <li><a href="converted_template/index.php">Converted Template</a></li>
+        </ul>
+    </nav>
+
+    <p>This is the homepage of Healthy Life Clinic's web application.</p>
+
+    <h2>Navigation</h2>
+    <ul>
+        <li><a href="List_appointments.php">List Appointments</a></li>
+        <li><a href="Make_appointment.php">Make an Appointment</a></li>
+        <li><a href="edit_appointment.php">Edit an Appointment</a></li>
+        <li><a href="delete_appointment.php">Delete an Appointment</a></li>
+        <li><a href="view_appointment.php">View an Appointment</a></li>
+    </ul>
+</body>
+</html>
